2a0b48fa2c7f73a49b4baa6942e9d33e473d03ff
athaarvva <92917528+athaarvva@users.noreply.github.com>
2022-08-08 15:56:40 +0530

    Create menu.c

---
+++ menu.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+int display()
+{   
+    printf("Select your designation : 1: Registrar 2:Lawyer 3:Judge")
+    int x;
+    scanf("%d",&x)
+    switch(x)
+    {
+    case 1:
+        {
+            printf("Enter Registrar Login Credentials : ");//Accepting registrar details and comparing the id pass with predeclared strings
+            char reg_user[10];
+            scanf("%c",&reg_user);
+            printf("Enter Password");
+            char reg_pass=[10];
+            scanf("%c",&reg_pass);
+            
+        }
+    case 2:
+        {   while(n==0)
+            {
+            printf("Enter Lawyer User ID and Password");
+            char law_user[10];
+            scanf("%c",&law_user);
+            printf("Enter Password");
+            char law_pass=[10];
+            scanf("%c",&law_pass);
+            
+                
+            
+                if(law_pass=="Lawyer@123")
+                {
+                    printf("Please enter the CIN ");
+                    scanf("%c",&law_pass);
+                    
+                
+                }
+                    else()
+                {
+                        printf("Wrong Password");
+                        
+                }
+            }
+                
+        }
+        
+    }
+}
